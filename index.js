cc0e78d23464f779adda34ae05fdc26a998ce3a5
sachincalicut <sachisavera@gmail.com>
2020-07-12 00:45:25 +0530

    one list modified

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // Import stylesheets
 import './style.css';
-
+var myArray = [20, 40, 80, 100];
+var revMyArr = [].concat(myArray).reverse();
+console.log(revMyArr);
+// [100, 80, 40, 20]
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
